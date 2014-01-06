b00c94e6bc5c83dcd54af463772bf437c3c66502
Brett Smith <brett@w3.org>
2014-01-06 17:01:25 -0500

    setup: Add initial setup.py.

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#
+# Copyright © 2013-2014 World Wide Web Consortium, (Massachusetts
+# Institute of Technology, European Research Consortium for
+# Informatics and Mathematics, Keio University, Beihang). All Rights
+# Reserved. This work is distributed under the W3C® Software License
+# [1] in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.
+#
+# [1] http://www.w3.org/Consortium/Legal/2002/copyright-software-20021231
+#
+# Written January 2014 by Brett Smith <brett@w3.org>
+# This module depends on the third-party pyinotify module.
+
+from distutils.core import setup
+
+from limitfiles import VERSION
+URL = "http://www.github.com/w3c/limitfiles"
+
+setup(name="limitfiles",
+      version=VERSION,
+      description="Use inotify to automatically clean files",
+      author="Brett Smith",
+      author_email="brett@w3.org",
+      url=URL,
+      download_url=URL,
+      py_modules=['limitfiles'],
+      license="W3C Software Notice and License"
+      )
